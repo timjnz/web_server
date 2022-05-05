32c2a09d5fd46512dc0e55d1472a0ddde4ff0b99
Tim Jones <timjonez@protonmail.com>
2022-05-05 13:55:18 -0500

    [src/main] add logic to serve 404 page if request does not match accepted route

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    let contents = fs::read_to_string("index.html").unwrap();
+
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
-
-    stream.read(&mut buffer).unwrap();
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
