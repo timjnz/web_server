0771e2af408e40379665d416e12a2b0b679aaa6e
Tim Jones <timjonez@protonmail.com>
2022-05-05 15:10:18 -0500

    [src/main] add thread pool for async handling

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::fs;
 use std::io::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
