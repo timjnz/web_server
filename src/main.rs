a7c77faa6f9a2201feee093b743b7723c0246dd6
Tim Jones <timjonez@protonmail.com>
2022-05-05 13:40:15 -0500

    [src/main] Add html file to response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -13,7 +14,12 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.read(&mut buffer).unwrap();
     stream.write(response.as_bytes()).unwrap();
